8415e403c762215cce88a5eeb75cd92d7cf2ee61
Ankit Kumar <heyankitkr@gmail.com>
2023-01-03 00:03:44 +0530

    pattern 7 updated

--- patterns/pattern7.py
+++ patterns/pattern7.py
@@ -5,13 +5,13 @@ def func(n):
     for i in range(n):
         # space
         for j in range(n - i - 1):
-            print(" ", end=" ")
+            print(" ", end="")
         # star
         for j in range(2 * i + 1):
-            print("*", end=" ")
+            print("*", end="")
         # space
         for j in range(n - i - 1):
-            print(" ", end=" ")
+            print(" ", end="")
         print("", end="\n")
 
 
